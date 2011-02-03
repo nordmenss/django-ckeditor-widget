512ffa7a5d99c24574a47fe4c073c8d46e2d9578
nordmenss <nordmenss@gmail.com>
2011-02-03 13:12:24 +0200

    initial commit

---
+++ ckeditor_widget/widgets.py
@@ -0,0 +1,58 @@
+from django import forms
+from django.conf import settings
+from django.forms.widgets import flatatt
+from django.utils.encoding import smart_unicode
+from django.utils.html import escape
+from django.utils.simplejson import *
+from django.utils.safestring import mark_safe
+from ckeditor.widgets import CKEditorWidget
+from django.utils.translation import ugettext as _
+
+DEFAULT_CONFIG = {
+    'skin': 'office2003',
+    'toolbar': 'Basic',
+    'height': 291,
+    'width': 618,
+    'filebrowserWindowWidth': 940,
+    'filebrowserWindowHeight': 747,
+}
+
+class CKEditorWidget(forms.Textarea):
+    def __init__(self, config_name='default', attrs=None, mce_attrs=None):
+        super(CustomCKEditorWidget, self).__init__(attrs)
+        self.config_name=config_name
+
+    class Media:
+        try:
+            js = (
+                settings.CKEDITOR_MEDIA_PREFIX + 'ckeditor/ckeditor.js',
+            )
+        except AttributeError:
+            raise ImproperlyConfigured("django-ckeditor requires CKEDITOR_MEDIA_PREFIX setting. This setting specifies a URL prefix to the ckeditor JS and CSS media (not uploaded media). Make sure to use a trailing slash: CKEDITOR_MEDIA_PREFIX = '/media/ckeditor/'")
+
+    def render(self, name, value, attrs=None):
+        if value is None: value = ''
+        value = smart_unicode(value)
+        final_attrs = self.build_attrs(attrs, name=name)
+
+        self_config = DEFAULT_CONFIG
+        configs = getattr(settings, 'CKEDITOR_CONFIGS', None)
+        if configs != None:
+            if isinstance(configs, dict):
+                # Make sure the config_name exists.
+                if configs.has_key(self.config_name):
+                    config = configs[self.config_name].copy()
+                    # Make sure the configuration is a dictionary.
+                    if not isinstance(config, dict):
+                        raise ImproperlyConfigured('CKEDITOR_CONFIGS["%s"] setting must be a dictionary type.' % self.config_name)
+                    self_config.update(config)
+                else:
+                    raise ImproperlyConfigured("No configuration named '%s' found in your CKEDITOR_CONFIGS setting." % self.config_name)
+            else:
+                raise ImproperlyConfigured('CKEDITOR_CONFIGS setting must be a dictionary type.')
+
+        ck_json = JSONEncoder().encode(self_config)
+        return mark_safe(u'<textarea%s>%s</textarea> <script type="text/javascript" charset="utf-8"> \
+            CKEDITOR.replace( "%s", %s); \
+        </script>' % (flatatt(final_attrs), escape(value),'id_'+name,ck_json)
+        )
